2a384daf43c3c9df196b6e618920ecb8fc24ef5b
TrkQuang <3124410292@sv.sgu.edu.vn>
2025-12-29 21:00:17 +0700

    Save local changes before push

--- src/main/java/phongkham/db/DBConnection.java
+++ src/main/java/phongkham/db/DBConnection.java
@@ -1,34 +1,35 @@
 package main.java.phongkham.db;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL  = "jdbc:mysql://localhost:3306/clinic_db?useSSL=false&serverTimezone=UTC";
-    private static final String USER = "qlpk";
-    private static final String PASS = "123456";
-    private static Connection connection = null;
-    // Ngăn tạo object mới (chỉ dùng static)
-    private DBConnection() {}
-    public static Connection getConnection() {
-        try {
-            if (connection == null || connection.isClosed()) {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                connection = DriverManager.getConnection(URL, USER, PASS);
-                System.out.println("Đã kết nối vào DATABASE");
-            }
-        } catch (SQLException | ClassNotFoundException e) {
-            System.out.println("❌ Kết nối database thất bại ");
-            e.printStackTrace();
-        }
-        return connection;
+
+  private static final String URL =
+    "jdbc:mysql://tramway.proxy.rlwy.net:57864/qlpk_db?useSSL=false&serverTimezone=UTC";
+  private static final String USER = "root";
+  private static final String PASS = "RSFJaDbgzwfGdkHBtEGwLPhFyXvemcGZ";
+
+  private static Connection connection = null;
+
+  // Không cho tạo object
+  private DBConnection() {}
+
+  public static Connection getConnection() {
+    try {
+      if (connection == null || connection.isClosed()) {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection(URL, USER, PASS);
+        System.out.println("✅ Kết nối MySQL Railway thành công");
+      }
+    } catch (ClassNotFoundException e) {
+      System.out.println("❌ Không tìm thấy MySQL Driver");
+      e.printStackTrace();
+    } catch (SQLException e) {
+      System.out.println("❌ Kết nối database thất bại");
+      e.printStackTrace();
     }
+    return connection;
+  }
 }
-
-
-
-
-
-
-
-
