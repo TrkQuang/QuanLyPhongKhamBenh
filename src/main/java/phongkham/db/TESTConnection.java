fb80bd755f17eaec7bd7b0aa99ae34ca996ec002
TrkQuang <3124410292@sv.sgu.edu.vn>
2026-01-01 11:42:15 +0700

    Update TESTConnection.java and add new library files

--- src/main/java/phongkham/db/TESTConnection.java
+++ src/main/java/phongkham/db/TESTConnection.java
@@ -4,6 +4,7 @@ public class TESTConnection {
     public static void main(String[] args) {
         if (DBConnection.getConnection() != null) {
             System.out.println("Kết nối OK");
+            DBConnection.closeConnection();
         } else {
             System.out.println("Kết nối FAIL");
         }
