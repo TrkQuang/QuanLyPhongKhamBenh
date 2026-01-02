3536cf8d490e9a0b8ede24b8181e37bc11dba0c6
Nhan0709 <nhan07092006@gmail.com>
2026-01-02 16:17:09 +0700

    Update BacSi and Khoa

--- src/main/java/phongkham/model/BacSi.java
+++ src/main/java/phongkham/model/BacSi.java
@@ -1,5 +1,46 @@
 package main.java.phongkham.model;
 
 public class BacSi {
+    private String maBS;
+    private String hoTen;
+    private String chuyenKhoa;
+    private String sdt;
+    private String chucVu;
+    private String hocVi;
+    private Khoa khoa;
+
+
+    public BacSi(String maBS, String hoTen, String chuyenKhoa,
+                 String sdt, String chucVu, String hocVi, Khoa khoa) {
+        this.maBS = maBS;
+        this.hoTen = hoTen;
+        this.chuyenKhoa = chuyenKhoa;
+        this.sdt = sdt;
+        this.chucVu = chucVu;
+        this.hocVi = hocVi;
+        this.khoa = khoa;
+    }
+        public BacSi() {
+        this.maBS = null;
+        this.hoTen = null;
+        this.chuyenKhoa = null;
+        this.sdt = null;
+        this.chucVu = null;
+        this.hocVi = null;
+        this.khoa = null;
+    }
+
+    public String getMaBS() {
+        return maBS;
+    }
+    public void setMaBS(String maBS) {
+        this.maBS = maBS;
+    }
     
+    @Override
+    public String toString() {
+        return this.maBS+" - "+this.hoTen+" - "+this.chuyenKhoa+" - "+this.sdt+" - "+
+               this.chucVu+" - "+this.hocVi+" - "+(khoa!=null?khoa.getMaKhoa():"-");
+    }
+
 }
--- src/main/java/phongkham/model/Khoa.java
+++ src/main/java/phongkham/model/Khoa.java
@@ -1,5 +1,24 @@
 package main.java.phongkham.model;
 
 public class Khoa {
-    
+    private String maKhoa;
+    private String tenKhoa;
+
+    public Khoa(String maKhoa, String tenKhoa){
+        this.maKhoa = maKhoa;
+        this.tenKhoa = tenKhoa;
+    }
+
+    public String getMaKhoa() {
+        return maKhoa;
+    }
+    public void setMaKhoa(String maKhoa) {
+        this.maKhoa = maKhoa;
+    }
+    public String getTenKhoa() {
+        return tenKhoa;
+    }
+    public void setTenKhoa(String tenKhoa) {
+        this.tenKhoa = tenKhoa;
+    }
 }
