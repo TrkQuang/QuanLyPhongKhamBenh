fc3bd467e1ff8ed4ac3e6a1f4f2bbde7a6168890
Baotris-23 <tri230320006@gmail.com>
2026-01-19 06:42:57 +0700

    Enhance GoiDichVuDTO with constructors and methods
    Add constructors and getter/setter methods for GoiDichVuDTO

--- src/main/java/phongkham/DTO/GoiDichVuDTO.java
+++ src/main/java/phongkham/DTO/GoiDichVuDTO.java
@@ -1,5 +1,36 @@
 package phongkham.DTO;
 
 public class GoiDichVuDTO {
-    
+    private String MaGoi;
+    private String TenGoi;
+    private String GiaDichVu;
+    private String ThoiGianKham;
+    private String MoTa;
+
+    public GoiDichVuDTO () {
+        MaGoi =" ";
+        TenGoi =" ";
+        GiaDichVu =" ";
+        ThoiGianKham =" ";
+        MoTa =" ";
+    }
+
+    public GoiDichVuDTO (String MaGoi, String TenGoi, String GiaDichVu, String ThoiGianKham, String MoTa) {
+        this.MaGoi=MaGoi;
+        this.TenGoi=TenGoi;
+        this.GiaDichVu=GiaDichVu;
+        this.ThoiGianKham=ThoiGianKham;
+        this.MoTa=MoTa;
+    }
+
+    public String getMaGoi() {return Magoi;}
+    public String getTenGoi() {return TenGoi;}
+    public String getGiaDichVu() {return GiaDichVu;}
+    public String getThoiGianKham() {return ThoiGianKham;}
+    public String getMoTa() {return MoTa;}
+
+    public void setMaGoi(String MaGoi ) {this.MaGoi=MaGoi;}
+    public void setTenGoi(String TenGoi ) {this.TenGoi=TenGoi;}
+    public void setGiaDichVu(String GiaDichVu ) {this.GiaDichVu=GiaDichVu;}
+    public void setThoiGianKham(String ThoiGianKham ) {this.ThoiGianKham=ThoiGianKham;}
 }
