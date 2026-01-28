86e0c2360be0274bbf82d52c4cc68fd3626ee400
Baotris-23 <tri230320006@gmail.com>
2026-01-28 13:34:38 +0700

    Implement DonThuocDAO methods for CRUD operations

--- src/main/java/phongkham/dao/DonThuocDAO.java
+++ src/main/java/phongkham/dao/DonThuocDAO.java
@@ -1,3 +1,66 @@
 package phongkham.dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import phongkham.db.DBConnection;
+import phongkham.DTO.DonThuocDTO;
 
-public class DonThuocDAO {}
+public class DonThuocDAO {
+  public ArrayList<DonThuocDTO> getAll() {
+        ArrayList<DonThuocDTO> ds = new ArrayList<>();
+        String sql = "SELECT * FROM DonThuoc";
+
+        try (
+            Connection c = DBConnection.getConnection();
+            Statement stm = c.createStatement();
+            ResultSet rs = stm.executeQuery(sql);
+        ) {
+            while (rs.next()) {
+                DonThuocDTO dt = new DonThuocDTO();
+                dt.setMaDonThuoc(rs.getString("MaDonThuoc"));
+                dt.setMaHoSo(rs.getString("MaHoSo"));
+                dt.setNgayKeDon(rs.getString("NgayKeDon"));
+                dt.setGhiChu(rs.getString("GhiChu"));
+                ds.add(dt);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ds;
+    }
+
+    public boolean insertDonThuoc(DonThuocDTO dt) {
+        String sql = "INSERT INTO DonThuoc VALUES(?,?,?,?)";
+        try (
+            Connection c = DBConnection.getConnection();
+            PreparedStatement ps = c.prepareStatement(sql);
+        ) {
+            ps.setString(1, dt.getMaDonThuoc());
+            ps.setString(2, dt.getMaHoSo());
+            ps.setString(3, dt.getNgayKeDon());
+            ps.setString(4, dt.getGhiChu());
+
+            return ps.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean deleteMaDonThuoc(String MaDonThuoc) {
+        String sql = "DELETE FROM DonThuoc WHERE MaDonThuoc=?";
+        try (
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+        ) {
+            ps.setString(1, MaDonThuoc);
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
